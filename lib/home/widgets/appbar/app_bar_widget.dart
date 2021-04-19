a5cf50fe95434b890bddd708b5ae16d68f054d8c
John Emerson <john.carv.sousa@gmail.com>
2021-04-19 19:38:26 -0300

    feat: finish first day home

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,5 +1,5 @@
-import 'package:devquiz/core/app_gradients.dart';
 import 'package:devquiz/core/core.dart';
+import 'package:devquiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -7,42 +7,48 @@ class AppBarWidget extends PreferredSize {
       : super(
           preferredSize: Size.fromHeight(250),
           child: Container(
-            decoration: BoxDecoration(gradient: AppGradients.linear),
             height: 250,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text.rich(
-                    TextSpan(
-                      text: 'Olá',
-                      style: AppTextStyles.title,
-                      children: [
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  height: 161,
+                  width: double.maxFinite,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
                         TextSpan(
-                          text: 'John Emerson',
-                          style: AppTextStyles.titleBold,
+                          text: 'Olá, ',
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: 'John Emerson',
+                              style: AppTextStyles.titleBold,
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                  Text(
-                    'Olá, John Emerson',
-                    style: AppTextStyles.title,
-                  ),
-                  Container(
-                    width: 58,
-                    height: 58,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            'https://avatars.githubusercontent.com/u/43749971?v=4'),
                       ),
-                    ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                'https://avatars.githubusercontent.com/u/43749971?v=4'),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                ),
+              ],
             ),
           ),
         );
