e30f0d0cb511b8dbe4bf393ec1eb84be9e7f014e
John Emerson <john.carv.sousa@gmail.com>
2021-04-21 20:41:10 -0300

    feat: add answer widget

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,3 +1,4 @@
+import 'package:devquiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:devquiz/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +18,22 @@ class QuizWidget extends StatelessWidget {
           SizedBox(
             height: 24,
           ),
+          AnswerWidget(
+              title:
+                  'Possibilita a criação de aplicativos compilados nativamente.'),
+          AnswerWidget(
+              isRight: true,
+              isSelected: true,
+              title:
+                  'Possibilita a criação de aplicativos compilados nativamente.'),
+          AnswerWidget(
+              isRight: false,
+              isSelected: true,
+              title:
+                  'Possibilita a criação de aplicativos compilados nativamente.'),
+          AnswerWidget(
+              title:
+                  'Possibilita a criação de aplicativos compilados nativamente.'),
         ],
       ),
     );
