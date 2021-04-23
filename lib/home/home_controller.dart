b2937f2e827b07478db977f7e1ad03f1fdf6a40b
John Emerson <john.carv.sousa@gmail.com>
2021-04-23 19:37:19 -0300

    feat: add setState with ValueNotifier

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -4,21 +4,32 @@ import 'package:devquiz/shared/models/answer_model.dart';
 import 'package:devquiz/shared/models/question_model.dart';
 import 'package:devquiz/shared/models/quiz_model.dart';
 import 'package:devquiz/shared/models/user_model.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeController {
-  HomeState state = HomeState.empty;
+  final stateNotifier = ValueNotifier<HomeState>(HomeState.empty);
+
+  set state(HomeState state) => stateNotifier.value = state;
+  HomeState get state => stateNotifier.value;
 
   UserModel? user;
   List<QuizModel>? quizzes;
 
-  void getUser() {
+  void getUser() async {
+    state = HomeState.loading;
+    await Future.delayed(Duration(seconds: 2));
+
     user = UserModel(
       name: 'John Emerson',
       photoUrl: 'https://avatars.githubusercontent.com/u/43749971?v=4',
     );
+    state = HomeState.success;
   }
 
-  void getQuizzes() {
+  void getQuizzes() async {
+    state = HomeState.loading;
+    await Future.delayed(Duration(seconds: 2));
+
     quizzes = [
       QuizModel(
         title: 'NLW 5 Flutter',
@@ -34,5 +45,6 @@ class HomeController {
         level: Level.facil,
       )
     ];
+    state = HomeState.success;
   }
 }
