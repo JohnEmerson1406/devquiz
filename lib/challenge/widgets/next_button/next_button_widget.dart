e0b6fa5ec70eeaa5e0f068fb5486c8b1d8c8a0af
John Emerson <john.carv.sousa@gmail.com>
2021-04-25 14:35:46 -0300

    feat: add void callback to button

--- lib/challenge/widgets/next_button/next_button_widget.dart
+++ lib/challenge/widgets/next_button/next_button_widget.dart
@@ -6,20 +6,32 @@ class NextButtonWidget extends StatelessWidget {
   final String label;
   final Color backgroundColor;
   final Color fontColor;
+  final Color borderColor;
+  final VoidCallback onTap;
   NextButtonWidget({
     required this.label,
     required this.backgroundColor,
     required this.fontColor,
+    required this.borderColor,
+    required this.onTap,
   });
 
-  NextButtonWidget.green({required String label})
-      : this.backgroundColor = AppColors.darkGreen,
+  NextButtonWidget.green({
+    required String label,
+    required VoidCallback onTap,
+  })   : this.backgroundColor = AppColors.darkGreen,
         this.fontColor = AppColors.white,
+        this.borderColor = AppColors.green,
+        this.onTap = onTap,
         this.label = label;
 
-  NextButtonWidget.white({required String label})
-      : this.backgroundColor = AppColors.white,
+  NextButtonWidget.white({
+    required String label,
+    required VoidCallback onTap,
+  })   : this.backgroundColor = AppColors.white,
         this.fontColor = AppColors.grey,
+        this.borderColor = AppColors.border,
+        this.onTap = onTap,
         this.label = label;
 
   @override
@@ -34,8 +46,9 @@ class NextButtonWidget extends StatelessWidget {
               borderRadius: BorderRadius.circular(10),
             ),
           ),
+          side: MaterialStateProperty.all(BorderSide(color: borderColor)),
         ),
-        onPressed: () {},
+        onPressed: onTap,
         child: Text(
           label,
           style: GoogleFonts.notoSans(
